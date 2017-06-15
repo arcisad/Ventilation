363177076ccc5beb399b9bdd14c5929e0782e1e8
arcisad <amir.rastar@gmail.com>
2017-06-16 09:34:08 +1200

    Added calculation tests

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,13 +1,20 @@
 import unittest
 import inspect
 
+
 from sources import ven
-from ven import TLC, FRC
+from ven import FRC, TLC
 
 
 class ApplicationTests(unittest.TestCase):
     def test_application(self):
         self.assertTrue(inspect.ismodule(ven))
+
+    def test_calculate(self):
+        value1 = float(FRC.get())
+        value2 = float(TLC.get())
+        vc = value2 - value1/2
+        self.assertEqual(vc, 6)
 
 if __name__ == '__main__':
     unittest.main()
